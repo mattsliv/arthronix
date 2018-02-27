677a08c7ad163dfba5f4821fd65325c45f0d2c42
savannaos <savanna.smith@uconn.edu>
2018-02-26 21:48:23 -0500

    deleted outdated charts

---
+++ database/add_stats.sql
@@ -0,0 +1,36 @@
+/*Chris's entry for feb 8*/
+INSERT INTO Statistics (UserId, DateRecorded, ROM, Strength, PEG)
+VALUES ('002', '02/08/18', '10.4', '5.0', '9.5');
+
+INSERT INTO Statistics (UserId, DateRecorded, ROM, Strength, PEG)
+VALUES ('002', '02/12/18', '14.4', '7.0', '8.5');
+
+INSERT INTO Statistics (UserId, DateRecorded, ROM, Strength, PEG)
+VALUES ('002', '02/22/18', '20.2', '8.1', '6.0');
+
+INSERT INTO Statistics (UserId, DateRecorded, ROM, Strength, PEG)
+VALUES ('002', '02/28/18', '40.4', '10.0', '4.5');
+
+/*stats for shemona*/
+INSERT INTO Statistics (UserId, DateRecorded, ROM, Strength, PEG)
+VALUES ('003', '02/12/18', '74.4', '7.0', '5.5');
+
+INSERT INTO Statistics (UserId, DateRecorded, ROM, Strength, PEG)
+VALUES ('003', '02/22/18', '80.2', '8.1', '6.0');
+
+/*Savanna's entry for feb 8*/
+INSERT INTO Statistics (UserId, DateRecorded, ROM, Strength, PEG)
+VALUES ('004', '02/08/18', '60.4', '5.0', '4.5');
+
+INSERT INTO Statistics (UserId, DateRecorded, ROM, Strength, PEG)
+VALUES ('004', '02/12/18', '74.0', '7.0', '3.5');
+
+/*Matt's entry for feb 8*/
+INSERT INTO Statistics (UserId, DateRecorded, ROM, Strength, PEG)
+VALUES ('005', '02/01/18', '78.0', '5.0', '4.5');
+
+INSERT INTO Statistics (UserId, DateRecorded, ROM, Strength, PEG)
+VALUES ('005', '02/08/18', '85.4', '7.0', '5.5');
+
+INSERT INTO Statistics (UserId, DateRecorded, ROM, Strength, PEG)
+VALUES ('005', '02/22/18', '90.0', '8.1', '6.0');
