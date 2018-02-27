f58f1225693f5aeb5c99b319b55c23c8474e4b77
savannaos <savanna.smith@uconn.edu>
2018-02-27 13:43:24 -0500

    cleaned up charts

--- provider/src/PatientDash/Charts.js
+++ provider/src/PatientDash/Charts.js
@@ -1,17 +1,6 @@
 import React, { Component } from 'react'
 import {Bar} from 'react-chartjs';
 
-var data = {
-  labels: ["Mon", "Tues", "Weds", "Thurs", "Fri", "Sat", "Sun"], //x-axis are dates
-  datasets: [{
-          label: 'Strength',
-          data: [2, 2, 4, 3, 5, 4, 5.5], //y-axis
-          backgroundColor:'rgba(75, 192, 192, 0.2)',
-          borderColor:'rgba(153, 102, 255, 1)',
-          borderWidth: 1
-      }]
-};
-
 var options = {
     scales: {
         yAxes: [{
