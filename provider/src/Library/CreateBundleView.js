4a343ee9e0d1cc62623187d55174a6e8c5576065
Elaine Tsun <elaine@Elaines-MacBook-Pro.local>
2018-03-08 11:23:02 -0500

    Added Create Bundle table

--- provider/src/Library/CreateBundleView.js
+++ provider/src/Library/CreateBundleView.js
@@ -7,10 +7,50 @@ import {
 
 
 class BundleMatrix extends Component {
+
   render() {
     return (
       <div>
         <h1>Create Bundle</h1>
+          <div class = "table-responsive">
+            <table id="bundleTable" class="table" data-sort="table">
+            <thead>
+              <tr>
+                <th>Select</th>
+                <th>Exercise Name</th>
+                <th>Sets</th>
+                <th>Reps</th>
+              </tr>
+            </thead>
+            <tbody class="dash-table">
+              <tr>
+                <td>
+                <form>
+        <div className="radio">
+          <label>
+            <input type="radio" value="option1" checked={true}/>
+            L
+          </label>
+        </div>
+        <div className="radio">
+          <label>
+            <input type="radio" value="option2" />
+            M
+          </label>
+        </div>
+        <div className="radio">
+          <label>
+            <input type="radio" value="option3" />
+            H
+          </label>
+        </div>
+      </form>
+                </td>
+
+              </tr>
+              </tbody>
+            </table>
+          </div>
       </div>
     )
   }
