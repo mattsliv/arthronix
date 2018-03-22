61b4055d96a4050b10124175744df8d749911ef2
savannaos <savanna.smith@uconn.edu>
2018-03-21 21:26:16 -0400

    order exercises

--- provider/src/Library/CreateBundleView.js
+++ provider/src/Library/CreateBundleView.js
@@ -5,9 +5,9 @@ class SelectLevelBox extends Component {
   constructor(props) {
     super(props);
     this.handleOptionChange = this.handleOptionChange.bind(this);
-    this.state = {
-      selectedOption: 'option1'
-    }
+      this.state = {
+        selectedOption: props.level
+      }
   }
 
   handleOptionChange(changeEvent) {
@@ -20,15 +20,15 @@ class SelectLevelBox extends Component {
     return (
     <div>
       <div class="form-check form-check-inline">
-        <input class="form-check-input" type="radio" value="option1" checked={this.state.selectedOption === 'option1'} onChange={this.handleOptionChange} />
+        <input class="form-check-input" type="radio" value="low" checked={this.state.selectedOption === 'low'} onChange={this.handleOptionChange} />
         <label class="form-check-label">L </label>
       </div>
       <div class="form-check form-check-inline">
-        <input class="form-check-input" type="radio" value="option2" checked={this.state.selectedOption === 'option2'} onChange={this.handleOptionChange} />
+        <input class="form-check-input" type="radio" value="med" checked={this.state.selectedOption === 'med'} onChange={this.handleOptionChange} />
         <label class="form-check-label">M </label>
       </div>
       <div class="form-check form-check-inline">
-        <input class="form-check-input" type="radio" value="option3" checked={this.state.selectedOption === 'option3'} onChange={this.handleOptionChange} />
+        <input class="form-check-input" type="radio" value="high" checked={this.state.selectedOption === 'high'} onChange={this.handleOptionChange} />
         <label class="form-check-label">H </label>
       </div>
     </div>
@@ -55,8 +55,35 @@ class BundleMatrix extends Component {
 
   render() {
     let titles = ["Select", "Exercise", "Sets", "Reps"];
-    let rows   = this.state.exercises.map(e => e.exname);
-    rows       = rows.filter(word => word); //remove null entries
+    let rows   = this.state.exercises.map(ex => {
+      if(ex.id == 2 || ex.id == 6) { //from here
+        var rEx = {
+          key: ex.id,
+          exname: ex.exname,
+          level: "low",
+          reps: "6-8",
+          sets: "2-3"
+        };
+        return rEx;
+      }                          /// to here will be replaced by loading exercises already in bundle
+      else {
+        var rEx = {
+          key: ex.id,
+          exname: ex.exname,
+          level: "-",
+          reps: "-",
+          sets: "-"
+        };
+        return rEx;
+      }
+      })
+    rows = rows.filter(word => word); //remove null entries
+    rows.sort(function(a,b){                //sort by which exercises are selected or not
+      if(a.level == b.level) return 0;
+      if(a.level == "-") return 1;
+      else return -1;
+    });
+
     return (
       <div>
         <h1>Create Bundle</h1>
@@ -70,10 +97,10 @@ class BundleMatrix extends Component {
             <tbody class="dash-table">
             {rows.map(row =>
               <tr>
-                <td><SelectLevelBox levelbox/></td>
-                <td><p>{row}</p></td>
-                <td><p>3</p></td>
-                <td><p>5</p></td>
+                <td><SelectLevelBox level={row.level}/></td>
+                <td><p>{row.exname}</p></td>
+                <td><p>{row.sets}</p></td>
+                <td><p>{row.reps}</p></td>
               </tr>
             )}
               </tbody>
