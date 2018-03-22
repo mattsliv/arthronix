1ce8ca554414d7fa655260ea7cc611ecd0f8605d
Elaine Tsun <elaine.tsun3@gmail.com>
2018-03-22 10:35:49 -0400

    added submit and cancel buttons

--- provider/src/Library/CreateBundleView.js
+++ provider/src/Library/CreateBundleView.js
@@ -106,6 +106,9 @@ class BundleMatrix extends Component {
               </tbody>
             </table>
           </div>
+          <p align = "right"><button type="button" class="btn btn-primary" > Cancel </button>
+             <button type="button" class="btn btn-primary" > Submit </button>
+          </p>
       </div>
     )
   }
