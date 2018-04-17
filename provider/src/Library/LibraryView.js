e970f3e92d8d2bccb58302a1c48ac472a729c70a
Elaine Tsun <elaine.tsun3@gmail.com>
2018-04-17 12:30:19 -0400

    added pdf rendering, install react-pdf module

--- provider/src/Library/LibraryView.js
+++ provider/src/Library/LibraryView.js
@@ -1,4 +1,4 @@
-import React , { Component } from 'react'
+import React , { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -8,38 +8,111 @@ import {
 
 import Modal from 'react-modal'
 import pdf from '../images/pdf.png';
+import pdfFile from './PDFs/leg.pdf'
+
 import CreateBundle from './CreateBundleView'
+import {Document,Page} from 'react-pdf'
+
+class PdfView extends Component {
+constructor (props){
+    super();
+    this.state = {
+      numPages: 2,
+      pageNumber: 1,
+    };
+
+    this.onDocumentLoad = this.onDocumentLoad.bind(this);
 
 
-class SupportRow extends Component { /* display educational support matieral*/
+}
+
+  onDocumentLoad = ({numPages}) => {
+    this.setState({ numPages });
+  }
+  onPageChange= ({pageNumber}) => {
+    this.setState({pageNumber: 2});
+  }
+
+
+
   render() {
+    const {pageNumber, numPages} = this.state;
+
     return (
-      <div>
-        <table>
-          <tr padding="20px">
-            <td><h3>Educational Support</h3></td>
-            <td>
-              <button type="button" class="btn btn-primary" >
-               + Add New
-              </button>
-            </td>
-            <td>
-              <button type="button" class="btn btn-primary" >
-                Expand All
-              </button>
-            </td>
-          </tr>
-
-          <a href="PDFs/paper.pdf"><img class="pdf-image" src={pdf} alt="pdf"/></a>
-          <a href="PDFs/paper.pdf"><img class="pdf-image" src={pdf} alt="pdf"/></a>
-          <a href="PDFs/paper.pdf"><img class="pdf-image" src={pdf} alt="pdf"/></a>
-          <a href="PDFs/paper.pdf"><img class="pdf-image" src={pdf} alt="pdf"/></a>
-          <a href="PDFs/paper.pdf"><img class="pdf-image" src={pdf} alt="pdf"/></a>
-
-        </table>
+      <div align = 'center'>
+        <p>Page {this.state.pageNumber} of {this.state.numPages}</p>
+        <Document file = {pdfFile} onLoadSucess={this.onDocumentLoad}>
+            <Page pageNumber = {pageNumber} />
+        </Document>
+        <button type = 'button' onClick = {this.onPageChange}>NEXT</button>
       </div>
     )
   }
+}
+
+class SupportRow extends Component { /* display educational support matieral*/
+
+
+   constructor (props) {
+      super();
+      this.state = {
+       showModal: false
+
+      };
+
+      this.handleOpenModal = this.handleOpenModal.bind(this);
+      this.handleCloseModal = this.handleCloseModal.bind(this);
+
+    }
+
+    handleOpenModal () {
+      this.setState({ showModal: true });
+    }
+
+    handleCloseModal () {
+      this.setState({ showModal: false });
+    }
+
+
+  getModal () {
+      return (
+      <Modal isOpen = {this.state.showModal}>
+           <text style={{color: 'black'}}> <font size = '5'> Viewing PDF </font></text>
+           <PdfView />
+           <div align = 'right'>
+            <button type="button" class="btn btn-primary" onClick = {this.handleCloseModal}> Close </button>
+          </div>
+      </Modal> )
+    }
+
+    render() {
+      return (
+        <div>
+          <table>
+            <tr padding="20px">
+              <td><h3>Educational Support</h3></td>
+              <td>
+                <button type="button" class="btn btn-primary" onClick = {this.handleOpenModal} >
+                 + Add New
+                </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-primary" >
+                  Expand All
+                </button>
+              </td>
+            </tr>
+            {this.getModal()}
+            <img src = {pdf} class = 'pdf-image' onClick = {this.handleOpenModal} />
+            <img src = {pdf} class = 'pdf-image' onClick = {this.handleOpenModal} />
+            <img src = {pdf} class = 'pdf-image' onClick = {this.handleOpenModal} />
+            <img src = {pdf} class = 'pdf-image' onClick = {this.handleOpenModal} />
+            <img src = {pdf} class = 'pdf-image' onClick = {this.handleOpenModal} />
+
+          </table>
+        </div>
+      )
+    }
 }
 
 class VideoRow extends Component { /* display exercise video content */
