aa9b7842106f68b0eda2b09631498c57b613ff32
savannaos <savanna.smith@uconn.edu>
2018-04-18 11:32:10 -0400

    shifted submit and edit buttons

--- provider/src/Library/CreateBundleView.js
+++ provider/src/Library/CreateBundleView.js
@@ -197,8 +197,8 @@ class BundleMatrix extends Component {
                 {bundle.map(ex => this.showGreyEx(ex))}
               </tbody>
             </table>
-            <button type="button" class="btn btn-primary" onClick = {this.onSubmit} style = {{position: 'fixed', bottom: '50px', right: '100px'}} > Submit </button>
-            <button type="button" class="btn btn-primary" onClick = {this.onCancel} style = {{position: 'fixed', bottom: '50px', right: '50px'}} > Cancel </button>
+            <button type="button" class="btn btn-primary" onClick = {this.onSubmit} style = {{position: 'fixed', bottom: '50px', right: '110px'}} > Submit </button>
+            <button type="button" class="btn btn-primary" onClick = {this.onCancel} style = {{position: 'fixed', bottom: '50px', right: '60px'}} > Cancel </button>
           </div>
       </div>
     )
@@ -224,8 +224,8 @@ class BundleMatrix extends Component {
              })}
               </tbody>
             </table>
-            <button type="button" class="btn btn-primary" onClick = {this.onEdit} style = {{position: 'fixed', bottom: '50px', right: '50px'}}> Edit </button>
-            <button type="button" class="btn btn-primary" onClick = {this.onCancel} style = {{position: 'fixed', bottom: '50px', right: '90px'}}> Close </button>
+            <button type="button" class="btn btn-primary" onClick = {this.onEdit} style = {{position: 'fixed', bottom: '50px', right: '60px'}}> Edit </button>
+            <button type="button" class="btn btn-primary" onClick = {this.onCancel} style = {{position: 'fixed', bottom: '50px', right: '100px'}}> Close </button>
           </div>
       </div>
     )
